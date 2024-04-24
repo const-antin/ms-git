a2c13ee14aee98c72284262382c16d0ec767ca8b
Konstantin Hoßfeld <konstantin@hossfeld.cc>
2024-04-24 19:14:43 +0200

    Add comment, change number of input layers Our problem has more input variables than originally anticipated. This is why I changed the input neuron count from 10 to 15.

--- nn.py
+++ nn.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class SimpleNN(nn.Module):
     def __init__(self, input_size, hidden_size, hidden_size_2, output_size):
         super(SimpleNN, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc1 = nn.Linear(input_size, hidden_size) # NN linear layer
         self.fc2 = nn.Linear(hidden_size, hidden_size_2)
         self.fc3 = nn.Linear(hidden_size_2, output_size)
         
@@ -16,7 +16,7 @@ class SimpleNN(nn.Module):
         return x
 
 # Example usage:
-input_size = 10
+input_size = 15
 hidden_size = 20
 hidden_size_2 = 30
 output_size = 1  # For simplicity, assuming a single output node for regression task
